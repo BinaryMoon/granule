93ce95ea21f4f5230eb114f4c91c07814aaffc24
BinaryMoon <binarymoon@gmail.com>
2016-10-31 20:04:55 +0000

    improve default image attachment page design
    Includes fixing attachment description and caption display.

--- granule/image.php
+++ granule/image.php
@@ -14,17 +14,26 @@
  */
 
 	get_header();
+
+	if ( have_posts() ) {
+
 ?>
 
 	<main class="main-content content-single" role="main">
+
+<?php
+		while ( have_posts() ) {
+
+			the_post();
+?>
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 			<header class="entry-header">
 
 <?php
-	the_title( '<h1 class="entry-title">', '</h1>' );
-	get_template_part( 'parts/post-meta' );
+			the_title( '<h1 class="entry-title">', '</h1>' );
+			get_template_part( 'parts/post-meta' );
 ?>
 
 			</header>
@@ -35,33 +44,53 @@
 					<?php echo wp_get_attachment_link( get_the_ID(), 'granule-attachment-fullsize' ); ?>
 				</div>
 
+				<div class="attachment-description">
 <?php
-	the_content();
+			if ( has_excerpt() ) {
+?>
+					<div class="attachment-caption">
+						<?php the_excerpt(); ?>
+					</div>
+<?php
+			}
 
-	if ( $post->post_parent ) {
+			the_content();
+?>
+				</div>
+<?php
+			if ( $post->post_parent ) {
 ?>
 
 				<nav id="image-navigation" class="navigation image-navigation" role="navigation">
 					<div class="nav-links">
-						<span class="nav-parent"><a href="<?php echo esc_url( get_permalink( $post->post_parent ) ); ?>" rev="attachment" class="attachment-parent"><?php esc_html_e( '&lsaquo; Return to post', 'granule' ); ?></a></span>
-						<span class="nav-previous"><?php previous_image_link( false, esc_html__( 'Previous Image', 'granule' ) ); ?></span>
-						<span class="nav-next"><?php next_image_link( false, esc_html__( 'Next Image', 'granule' ) ); ?></span>
+						<span class="nav-parent">
+							<a href="<?php echo esc_url( get_permalink( $post->post_parent ) ); ?>" rev="attachment" class="attachment-parent"><?php esc_html_e( '&lsaquo; Return to post', 'granule' ); ?></a>
+						</span>
+						<span class="nav-previous">
+							<?php previous_image_link( false, esc_html__( 'Previous Image', 'granule' ) ); ?>
+						</span>
+						<span class="nav-next">
+							<?php next_image_link( false, esc_html__( 'Next Image', 'granule' ) ); ?>
+						</span>
 					</div>
 				</nav>
 
 <?php
-	}
+			}
 ?>
 
 			</section>
 		</article>
 
 <?php
-	get_template_part( 'parts/comments' );
+			get_template_part( 'parts/comments' );
+
+		} // End while().
 ?>
 
 	</main>
 
 <?php
+	} // End if().
 
 	get_footer();
