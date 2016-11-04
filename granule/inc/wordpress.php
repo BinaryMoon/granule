12fb8dc8cc4c14b992d359ddf53043dd68dca715
BinaryMoon <binarymoon@gmail.com>
2016-11-04 17:08:16 +0000

    add resource hints for google fonts

--- granule/inc/wordpress.php
+++ granule/inc/wordpress.php
@@ -150,6 +150,31 @@ function granule_fonts() {
 
 
 /**
+ * Add preconnect for Google Fonts.
+ *
+ * @param array  $urls          URLs to print for resource hints.
+ * @param string $relation_type The relation type the URLs are printed.
+ * @return array URLs to print for resource hints.
+ */
+function granule_resource_hints( $urls, $relation_type ) {
+
+	if ( wp_style_is( 'granule-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+
+	}
+
+	return $urls;
+
+}
+
+add_filter( 'wp_resource_hints', 'granule_resource_hints', 10, 2 );
+
+
+/**
  * Set up all the theme properties and extras.
  *
  * Also adds fonts to editor styles {@see granule_fonts}.
