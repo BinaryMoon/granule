18a771ec0b091b3908f3a80860152c0ca1fa46a5
BinaryMoon <binarymoon@gmail.com>
2017-03-28 16:02:24 +0100

    tidy customize-preview js

--- assets/scripts/customizer-preview.js
+++ assets/scripts/customizer-preview.js
@@ -15,41 +15,72 @@
 
 ;( function( $, document ) {
 
-	$( document ).ready( function() {
+	$( document ).ready(
+		function() {
 
-		// Site title.
-		wp.customize( 'blogname', function( value ) {
-			value.bind( function( to ) {
-				$( '.site-title' ).text( to );
-			} );
-		} );
-
-		// Site description.
-		wp.customize( 'blogdescription', function( value ) {
-			value.bind( function( to ) {
-				$( '.site-description' ).text( to );
-			} );
-		} );
-
-		// Header text color.
-		wp.customize( 'header_textcolor', function( value ) {
-			value.bind( function( to ) {
-				if ( 'blank' === to ) {
-					$( '.masthead .site-title, .masthead .site-description' ).css( {
-						'clip': 'rect(1px, 1px, 1px, 1px)',
-						'position': 'absolute'
-					} );
-				} else {
-					$( '.masthead .site-title, .masthead .site-description' ).css( {
-						'clip': 'auto',
-						'position': 'relative'
-					} );
-					$( '.masthead .site-title, .masthead .site-title a, .masthead .site-title a:hover, .masthead p.site-description' ).css( {
-						'color': to
-					} );
+			// Site title.
+			wp.customize(
+				'blogname',
+				function( value ) {
+					value.bind(
+						function( to ) {
+							$( '.site-title' ).text( to );
+						}
+					);
 				}
-			} );
-		} );
-	} );
+			);
+
+			// Site description.
+			wp.customize(
+				'blogdescription',
+				function( value ) {
+					value.bind(
+						function( to ) {
+							$( '.site-description' ).text( to );
+						}
+					);
+				}
+			);
+
+			// Header text color.
+			wp.customize(
+				'header_textcolor',
+				function( value ) {
+
+					value.bind(
+						function( to ) {
+
+							if ( 'blank' === to ) {
+
+								$( '.masthead .site-title, .masthead .site-description' ).css(
+									{
+										'clip': 'rect(1px, 1px, 1px, 1px)',
+										'position': 'absolute'
+									}
+								);
+
+							} else {
+
+								$( '.masthead .site-title, .masthead .site-description' ).css(
+									{
+										'clip': 'auto',
+										'position': 'relative'
+									}
+								);
+
+								$( '.masthead .site-title, .masthead .site-title a, .masthead .site-title a:hover, .masthead p.site-description' ).css(
+									{
+										'color': to
+									}
+								);
+
+							}
+						}
+					);
+				}
+			);
+
+		}
+	);
 
 } )( jQuery, document );
