f6fad0da5c356c6f4d6561c3054655b77679ea90
BinaryMoon <binarymoon@gmail.com>
2017-05-15 14:00:33 +0100

    make credits more flexible
    allows footer credits, to be added/ replaced/ hidden through a
    WordPress filter.

--- footer.php
+++ footer.php
@@ -34,11 +34,30 @@
 ?>
 		</a>
 
-		<section class="footer-wrap" role="contentinfo">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'granule' ) ); ?>" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'granule' ); ?>" rel="generator"><?php printf( esc_html__( 'Proudly powered by %s', 'granule' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'granule' ), 'Granule', '<a href="https://prothemedesign.com/">Pro Theme Design</a>' ); ?>
+<?php
+
+	/**
+	 * Check to see if a custom credits option is set.
+	 * If custom credits are set then the filter should output the credits and
+	 * return a non false value. This will hide the default footer credits.
+	 */
+	if ( false === apply_filters( 'granule-credits', false ) ) {
+
+?>
+
+		<section id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'granule' ) ); ?>" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'granule' ); ?>" rel="generator"><?php printf( esc_html__( 'Proudly powered by %s', 'granule' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'granule' ), 'Granule', '<a href="https://prothemedesign.com/">Pro Theme Design</a>' ); ?>
+			</div>
 		</section>
+
+<?php
+
+	}
+
+?>
 
 	</footer>
 
