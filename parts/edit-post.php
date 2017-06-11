f75340ddeac15b3fc31f3ffd238360367e2db8c0
BinaryMoon <binarymoon@gmail.com>
2017-06-11 15:47:40 +0100

    improve edit post text

--- parts/edit-post.php
+++ parts/edit-post.php
@@ -11,8 +11,8 @@
 	edit_post_link(
 		sprintf(
 			wp_kses(
-				/* translators: %s: Name of current post */
-				__( 'Edit<span class="screen-reader-text"> %s</span>', 'granule' ),
+				/* translators: %s: Name of current post. Only visible to screenreaders. */
+				__( 'Edit <span class="screen-reader-text">%s</span>', 'granule' ),
 				array(
 					'span' => array(
 						'class' => array(),
