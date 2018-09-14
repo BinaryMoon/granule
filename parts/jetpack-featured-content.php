b6edbfc87d02ef9acca1d2195ac7670923fe13fe
BinaryMoon <binarymoon@gmail.com>
2018-09-14 21:49:39 +0100

    tidy codes

--- parts/jetpack-featured-content.php
+++ parts/jetpack-featured-content.php
@@ -10,26 +10,28 @@
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU Public License
  */
 
-	if ( granule_has_featured_posts() ) {
+	if ( ! granule_has_featured_posts() ) {
+		return;
+	}
 
-		$featured_posts = granule_get_featured_posts( 4 );
+	$featured_posts = granule_get_featured_posts( 4 );
+
 ?>
 
 	<section class="showcase">
 
 <?php
-		foreach ( $featured_posts as $post ) {
+	foreach ( $featured_posts as $post ) {
 
-			setup_postdata( $post );
+		setup_postdata( $post );
 
-			get_template_part( 'parts/content', 'slider' );
+		get_template_part( 'parts/content', 'slider' );
 
-		}
+	}
 ?>
 
 	</section>
 
 <?php
-		wp_reset_postdata();
 
-	}
+	wp_reset_postdata();
